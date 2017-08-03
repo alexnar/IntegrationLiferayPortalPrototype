1cceafa3c66bd6c322533005b77cfdcfdd144b7c
alexnar <sasha_nar@mail.ru>
2017-08-03 19:25:44 +0300

    ProjectManageImpl implements ProjectManage (now without implementation)

--- modules/ProjectManage/src/main/java/org/etan/portal/integration/prototype/projectmanage/service/impl/ProjectManageImpl.java
+++ modules/ProjectManage/src/main/java/org/etan/portal/integration/prototype/projectmanage/service/impl/ProjectManageImpl.java
@@ -1,5 +1,30 @@
 package org.etan.portal.integration.prototype.projectmanage.service.impl;
 
 
-public class ProjectManageImpl {
+import org.etan.portal.integration.prototype.projectmanage.service.ProjectManage;
+import org.etan.portal.integration.prototype.projectmanage.service.context.ProjectManageContext;
+import org.osgi.service.component.annotations.Component;
+
+@Component(
+        immediate = true,
+        service = ProjectManage.class
+)
+public class ProjectManageImpl implements ProjectManage {
+
+    // TODO implement methods
+
+    @Override
+    public void createProject(String projectName, ProjectManageContext projectManageContext) {
+
+    }
+
+    @Override
+    public void assignUser(long userId, ProjectManageContext projectManageContext) {
+
+    }
+
+    @Override
+    public void unassignUser(long userId, ProjectManageContext projectManageContext) {
+
+    }
 }
