c1b9eaf48fe0c7b222c1da19fc951b4b88f757cd
alexnar <sasha_nar@mail.ru>
2017-08-03 15:31:05 +0300

    Nexus Service interface in NexusService module added

---
+++ modules/NexusService/src/main/java/org/etan/portal/integration/prototype/nexusservice/service/impl/NexusServiceImpl.java
@@ -0,0 +1,38 @@
+package org.etan.portal.integration.prototype.nexusservice.service.impl;
+
+import org.etan.portal.integration.prototype.nexusservice.service.NexusService;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.List;
+
+@Component(
+        immediate = true,
+        property = {
+                // TODO: enter required service properties
+        },
+        service = NexusService.class
+)
+public class NexusServiceImpl implements NexusService {
+
+    // TODO implement methods
+
+    @Override
+    public String createRepository(String repositoryName) {
+        return null;
+    }
+
+    @Override
+    public void addUserToRepository(String userId, String repositoryId) {
+
+    }
+
+    @Override
+    public void deleteUserFromRepository(String userId, String repositoryId) {
+
+    }
+
+    @Override
+    public List<Object> getLastArtifacts(String repositoryId, int artifactsCount) {
+        return null;
+    }
+}
