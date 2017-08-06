15620ef72c3858bccd24fc4445a07f5c6ccadc3a
alexnar <sasha_nar@mail.ru>
2017-08-06 13:01:07 +0300

    NexusInfrastructureEntityImpl implemented, by delegating of NexusService

--- modules/BusinessLogicLayer/NexusInfrastructureEntityApiImpl/src/main/java/org/etan/portal/integration/nexusinfrastructureentityapiimpl/service/impl/NexusInfrastructureEntityImpl.java
+++ modules/BusinessLogicLayer/NexusInfrastructureEntityApiImpl/src/main/java/org/etan/portal/integration/nexusinfrastructureentityapiimpl/service/impl/NexusInfrastructureEntityImpl.java
@@ -1,9 +1,17 @@
 package org.etan.portal.integration.nexusinfrastructureentityapiimpl.service.impl;
 
+import com.liferay.expando.kernel.model.ExpandoBridge;
 import com.liferay.portal.kernel.model.User;
 import org.etan.portal.integration.infrastructureentityapi.service.InfrastructureEntity;
 import org.etan.portal.integration.infrastructureentityapi.service.exception.InfrastructureEntityException;
+import org.etan.portal.integration.nexusservice.service.NexusService;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 
 @Component(
@@ -14,24 +22,40 @@ import org.osgi.service.component.annotations.Component;
         service = InfrastructureEntity.class
 )
 public class NexusInfrastructureEntityImpl implements InfrastructureEntity {
+
+    private static final String USER_NEXUS_ID_FIELD = "nexusUserId";
+    private static final String NEXUS_REPOSITORY_NAME = "NEXUS REPOSITORY";
+
+    @Reference
+    private NexusService nexusService;
+
     @Override
     public String createInfrastructureEntityProject(String projectName) throws InfrastructureEntityException {
-        return null;
+        String repositoryId = nexusService.createRepository(projectName);
+        return repositoryId;
     }
 
     @Override
     public void assignUser(User user, String infrastructureEntityProjectId) throws InfrastructureEntityException {
-
+        // TODO: Check if it works.
+        ExpandoBridge userExpandoBridge = user.getExpandoBridge();
+        Serializable userNexusIdSerializable = userExpandoBridge.getAttribute(USER_NEXUS_ID_FIELD);
+        String userNexusId = (String) userNexusIdSerializable;
+        nexusService.addUserToRepository(userNexusId, infrastructureEntityProjectId);
     }
 
     @Override
     public void unassignUser(User user, String infrastructureEntityProjectId) throws InfrastructureEntityException {
-
+        // TODO: Check if it works.
+        ExpandoBridge userExpandoBridge = user.getExpandoBridge();
+        Serializable userNexusIdSerializable = userExpandoBridge.getAttribute(USER_NEXUS_ID_FIELD);
+        String userNexusId = (String) userNexusIdSerializable;
+        nexusService.deleteUserFromRepository(userNexusId, infrastructureEntityProjectId);
     }
 
     @Override
     public String getName() {
-        return null;
+        return NEXUS_REPOSITORY_NAME;
     }
 
     // TODO: implement methods
