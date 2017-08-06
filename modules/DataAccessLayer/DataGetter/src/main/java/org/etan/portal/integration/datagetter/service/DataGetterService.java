b600deb90924f5008c2af16c49fdbdb8b5d22bc9
alexnar <sasha_nar@mail.ru>
2017-08-06 21:13:48 +0300

    getDataFromUrlWithAuthorization method added and implemented

--- modules/DataAccessLayer/DataGetter/src/main/java/org/etan/portal/integration/datagetter/service/DataGetterService.java
+++ modules/DataAccessLayer/DataGetter/src/main/java/org/etan/portal/integration/datagetter/service/DataGetterService.java
@@ -1,6 +1,7 @@
 package org.etan.portal.integration.datagetter.service;
 
 
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.etan.portal.integration.datagetter.service.exception.DataHttpGetException;
 
 /**
@@ -20,4 +21,18 @@ public interface DataGetterService {
      *                                or problems while data reading
      */
     StringBuilder getDataFromUrl(String url) throws DataHttpGetException;
+
+    /**
+     * Get data from specified url with authorization.
+     * If process gone wrong throws DataHttpGetException.
+     *
+     * @param url specified url
+     * @return - StringBuilder, contains data that
+     *           was received by url
+     * @throws DataHttpGetException - if some problems with connection
+     *                                or wrong url specified
+     *                                or problems while data reading
+     */
+    StringBuilder getDataFromUrlWithAuthorization(
+            String url,  UsernamePasswordCredentials usernamePasswordCredentials) throws DataHttpGetException;
 }
--- modules/DataAccessLayer/DataGetter/src/main/java/org/etan/portal/integration/datagetter/service/impl/DataGetterServiceImpl.java
+++ modules/DataAccessLayer/DataGetter/src/main/java/org/etan/portal/integration/datagetter/service/impl/DataGetterServiceImpl.java
@@ -2,6 +2,10 @@ package org.etan.portal.integration.datagetter.service.impl;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import org.apache.http.Header;
+import org.apache.http.auth.AuthenticationException;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.auth.BasicScheme;
 import org.etan.portal.integration.datagetter.service.exception.DataHttpGetException;
 import org.etan.portal.integration.datagetter.service.DataGetterService;
 import org.apache.http.HttpEntity;
@@ -33,8 +37,11 @@ public class DataGetterServiceImpl implements DataGetterService {
     private static final String CONNECTION_PROBLEM_MESSAGE = "Probably you have problems with your connection";
     private static final String READING_PAGE_PROBLEM_MESSAGE = "Problems while reading page html";
     private static final String GET_DATA_ERROR = "Error getting page html";
+    private static final String AUTHORIZATION_ERROR = "Authorization failed";
+
 
     private static final Log logger = LogFactoryUtil.getLog(DataGetterServiceImpl.class);
+
 
     @Override
     public StringBuilder getDataFromUrl(String url) throws DataHttpGetException {
@@ -63,6 +70,48 @@ public class DataGetterServiceImpl implements DataGetterService {
         } catch (IOException e) {
             logger.warn(READING_PAGE_PROBLEM_MESSAGE, e);
             throw new DataHttpGetException(GET_DATA_ERROR);
+        }
+        return htmlResponse;
+    }
+
+    @Override
+    public StringBuilder getDataFromUrlWithAuthorization(
+            String url, UsernamePasswordCredentials usernamePasswordCredentials) throws DataHttpGetException {
+
+        if (url == null) {
+            return new StringBuilder();
+        }
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        Header authenticate = null;
+        try {
+            authenticate = new BasicScheme().authenticate(usernamePasswordCredentials, httpGet, null);
+        } catch (AuthenticationException e) {
+            logger.warn(AUTHORIZATION_ERROR, e);
+        }
+        if (authenticate != null) {
+            httpGet.addHeader(authenticate);
+        }
+        StringBuilder htmlResponse = new StringBuilder();
+
+        try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
+            HttpEntity httpEntity = response.getEntity();
+            InputStream connectInputStream = httpEntity.getContent();
+            InputStreamReader connectInputStreamReader = new InputStreamReader(connectInputStream);
+            BufferedReader bufferedReader = new BufferedReader(connectInputStreamReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                htmlResponse.append(line);
+            }
+        } catch (UnknownHostException e) {
+            logger.warn(WRONG_URL_MESSAGE, e);
+            throw new DataHttpGetException(GET_DATA_ERROR, e);
+        } catch (ClientProtocolException e) {
+            logger.warn(CONNECTION_PROBLEM_MESSAGE, e);
+            throw new DataHttpGetException(GET_DATA_ERROR, e);
+        } catch (IOException e) {
+            logger.warn(READING_PAGE_PROBLEM_MESSAGE, e);
+            throw new DataHttpGetException(GET_DATA_ERROR, e);
         }
         return htmlResponse;
     }
